c74c12cd2be6249d60aac1a7f701e17d57a797e3
Nicholl2 <nikolasanindya0@gmail.com>
2025-05-22 22:45:30 +0700

    Icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {FiMenu} from 'react-icons/fi';
+import {FiMenu, FiHeart, FiSearch, FiShoppingChart, FiUser} from 'react-icons/fi';
 
 const Header = () => {
     const [isScrolled, setIsScrolled] = useState (false)
@@ -38,11 +38,67 @@ const Header = () => {
                                     <FiMenu size={24}/>
                             </button>
                         </div>
+                        <div className='w-full md:flex-1 max-w-sm'>
+                            <div className='relative w-full'>
+                                <input
+                                 type = "text" 
+                                 placeholder='Search...'
+                                 className='w-full px-3 py-2 text-xs border border-gray-300 rounded-full
+                                 focus:outline-none focus:ring-2 focus:ring-gray-700'/>
+                            <button className='absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-800'
+                                aria-label='Search Button'>
+                                    <FiSearch size={16}/> 
+                            </button>
+                            </div>
+                        </div>
+                        <div className='flex items-center justify-end space-x-4 w-full md:w-auto'>
+                            <button className='relative p-2 text-gray-900 hover:text-shadow-white' aria-label='Wishlist'>
+                                <FiHeart size={20}/>
+                                <span className='absolute -top-1 -right-1 bg-white text-gray-600 text-xs *:rounded-full h-5 w-5 flex items-center justify-center'>
+                                    3
+                                </span>
+                            </button>
+                            <button className='relative p-2 text-gray-900 hover:text-shadow-white' aria-label='Cart'>
+                                <FiShoppingChart size={20}/>
+                                <span className='absolute -top-1 -right-1 bg-white text-gray-600 text-xs *:rounded-full h-5 w-5 flex items-center justify-center'>
+                                    5
+                                </span>
+                            </button>
+                            <button className='p-2 text-gray-500 hover:text-shadow-white' aria-label='User'>
+                                <FiUser size={20}/>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className='bg-gray-900'>
+                <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+                    <nav className='Hidden md:flex justify-center py-3'>
+                        <ul className='flex flex-wrap gap-x-6 text-sm font-medium text-shadow-white'>
+                            {navItems.map((item)=>(
+                                <li key={item.id}>
+                                    <a href={item.link} className='hover:text-gray-200 tran transition-colors'>
+                                        {item.name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    {isMobileMenuOpen && (
+                        <div className='md:hidden mt-2 bg-white rounded-lg shadow-md p-4 space-y-3 text-gray-900 text-center'>
+                            {
+                                navItems.map((item)=>(
+                                    <a key = {item.id} href={item.link} className='block hover:text-gray-500 text-sm font-medium'>
+                                        {item.name}
+                                    </a>
+                                ))
+                            }
+                        </div>
+                    )}
+                </div>
+            </div>
         </header>
-    )
-}
+    );
+};
 
 export default Header
